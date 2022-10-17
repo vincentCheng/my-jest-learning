1807690f88116a4e85c36bff506bd2f955390773
黄刚 <462256272@qq.com>
2022-10-17 10:00:06 +0800

    《静态检查工具》ESLint和Prettier等配置

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    "jest/globals": true,
+  },
+  // 开启插件
+  plugins: [],
+  // 继承别人写好的配置。
+  // 继承了plugin:@typescript-eslint/recommended，这样就不用写plugins和parser了。
+  extends: [
+    // ESLint
+    "eslint:recommended",
+    // TypeScript
+    "plugin:@typescript-eslint/recommended",
+    // Jest
+    "plugin:jest/recommended",
+    // React Testing Library
+    "plugin:testing-library/react",
+    // 这个要放在最后
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    // 关闭规则
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-non-explicit-any": "off",
+    "no-var": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "testing-library/no-dom-import": "off",
+    // 错误提示
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/valid-expect": "error",
+    "testing-library/await-async-query": "error",
+    "testing-library/no-await-sync-query": "error",
+    // 告警提示
+    "jest/no-disabled-tests": "warn",
+    "jest/prefer-to-have-length": "warn",
+    "testing-library/no-debugging-utils": "warn",
+  },
+};
